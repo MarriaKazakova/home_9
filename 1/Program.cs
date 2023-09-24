e39bc5477f72814158f250d956758864b7c4b288
Maria <ec39sp@yandex.ru>
2023-09-24 20:20:57 +0200

    1

---
+++ 1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите значение N:");
+        int N = int.Parse(Console.ReadLine());
+
+        PrintNumbers(N);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n > 0)
+        {
+            Console.Write(n + " ");
+            PrintNumbers(n - 1);
+        }
+    }
+}
