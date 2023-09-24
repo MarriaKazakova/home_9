88ec4e0e97a6869a9c2b9e8163a44090446296bb
Maria <ec39sp@yandex.ru>
2023-09-24 20:21:39 +0200

    2

---
+++ 2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static int SumMN(int m, int n)
+    {
+        if (m == n)
+        {
+            return m;
+        }
+        
+        else
+        {
+            return m + SumMN(m + 1, n);
+        }
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите значение M: ");
+        int m = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        
+        Console.WriteLine($"Сумма натуральных чисел в промежутке от {m} до {n} равна: {SumMN(m, n)}");
+
+        Console.ReadLine();
+    }
+}
